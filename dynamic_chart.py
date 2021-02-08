19858099bb3dacccd7ee00d881cf052b99a56ba0
jakubz <a5892731@o2.pl>
2021-02-08 15:33:23 +0100

    dynamic v 1.2

--- dynamic_chart.py
+++ dynamic_chart.py
@@ -1,7 +1,7 @@
 '''
 real time chart:
 
-comments: poor refreshment
+comments: poor refreshment, works very badly when calling two plots
 '''
 
 import time
@@ -15,8 +15,8 @@ class chart():
     #chart_data = {}
 
     def __init__(self):
-        x = [0 for i in range(self.data_list_bufor_len)]
-        y = [0 for i in range(self.data_list_bufor_len)]
+        x = [None, None]
+        y = [None, None]
         fig = plt.figure()
         ax = fig.add_subplot(111)
         self.chart_data = {"figure": fig, "axis": ax, "x-axis": x, "y-axis": y, "time": 0}
@@ -24,21 +24,22 @@ class chart():
     def figure_get_input(self, input):
         self.chart_data["time"] = time.process_time()
         self.chart_data["x-axis"].append(self.chart_data["time"])
-        self.chart_data["x-axis"].pop(0)
         self.chart_data["y-axis"].append(input)
-        self.chart_data["y-axis"].pop(0)
+        if self.chart_data["x-axis"].__len__() >= (self.data_list_bufor_len + 1):
+            self.chart_data["x-axis"].pop(0)
+            self.chart_data["y-axis"].pop(0)
 
     def figure_draw(self):
         self.chart_data["axis"].plot(self.chart_data["x-axis"], self.chart_data["y-axis"], color='red')
 
         self.chart_data["figure"].canvas.draw()
-        self.chart_data["figure"].canvas.set_window_title('SIN')
+        self.chart_data["figure"].canvas.set_window_title(self.chart_title)
         plt.title(label=self.chart_title, fontsize=15, color="black")
         self.chart_data["axis"].set_xlabel('time')
         self.chart_data["axis"].set_ylabel('amplitude')
         self.chart_data["axis"].grid(True)
         self.chart_data["figure"].show()
-        plt.pause(0.01)
+        plt.pause(0.005)
         self.chart_data["axis"].set_xlim(left=max(0, self.chart_data["time"] - self.chart_len),
                                         right=self.chart_data["time"] + 0)
 
--- main.py
+++ main.py
@@ -5,39 +5,37 @@ import dynamic_chart
 from static_chart import figure_2
 
 
-
-
-
-
 def main():
 
-#------------------------------------------------1
-    #figure_1() # realtime chart
-#------------------------------------------------1
+    def call_dynamic_chart():
+        chart = dynamic_chart.chart()
+        chart.chart_title = "1"
+        #chart2 = dynamic_chart.chart()   # works very badly when calling two plots
+        #chart2.chart_title = "2"
 
-#------------------------------------------------2
-#    chart_data = figure_1_1_init()
-#    while True:
-#        output = sin_wawe(10, 0, 10, time.process_time())  # amplitude, offset, period
-#        chart_data = figure_1_1_input(chart_data, output)
-#------------------------------------------------2
+        while True:
+            # sin_wawe(amplitude, offset, period, time)
+            output1 = sin_wawe(10, 0, 10, time.process_time())  # works ok
+            output2 = sin_wawe(10, 0, 1, time.process_time())  # bad refreshment # amplitude, offset, period
+            output3 = 2 * time.process_time() + 5
+            output4 = 3 * time.process_time() + 5
 
-#------------------------------------------------3
-    chart = dynamic_chart.chart()
-    while True:
-        output = sin_wawe(10, 0, 10, time.process_time())  # amplitude, offset, period
-        chart.figure_get_input(output)
-        chart.figure_draw()
-#------------------------------------------------3
+            chart.figure_get_input(output3)
+            chart.figure_draw()
 
-#------------------------------------------------4
-    #figure_2() # static chart
-#------------------------------------------------4
+            #chart2.figure_get_input(output4)
+            #chart2.figure_draw()
 
+    def call_static_chart():
+        figure_2()  # static chart
 
 
+# ------------------MAIN--------------------
 
 
+    call_dynamic_chart()
+
+    #call_static_chart()
 
 
 
