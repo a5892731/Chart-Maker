d00b823984f81113e00de211da0a0138ea65ac2d
jakubz <a5892731@o2.pl>
2021-02-09 14:14:44 +0100

    dynamic v 1.2

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ def main():
 
     def call_dynamic_chart():
         chart = dynamic_chart.Chart()
-        chart.chart_title = "1"
+        chart.chart_title = "Chart 1"
         #chart2 = dynamic_chart.chart()   # works very badly when calling two plots
         #chart2.chart_title = "2"
 
         while True:
             # sin_wawe(amplitude, offset, period, time)
-            output1 = sin_wawe(10, 0, 10, time.process_time())  # works ok
+            output1 = sin_wawe(10, 0, 5, time.process_time())  # works ok
             output2 = sin_wawe(10, 0, 1, time.process_time())  # bad refreshment # amplitude, offset, period
             output3 = 2 * time.process_time() + 5
             output4 = 3 * time.process_time() + 5
 
-            chart.figure_get_input(output3)
+            chart.figure_get_input(output1)
             chart.figure_draw()
 
             #chart2.figure_get_input(output4)
@@ -36,9 +36,9 @@ def main():
 # ------------------MAIN--------------------
 
 
-    call_dynamic_chart()
+    #call_dynamic_chart()
 
-    #call_static_chart()
+    call_static_chart()
 
 
 
